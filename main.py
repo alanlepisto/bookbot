a48c5a2248afb934310292ce684de1f5a48d42d5
alanlepisto <alan@lepisto.dev>
2026-01-28 13:32:31 -0600

    Print a Report

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """Simple script to read Frankenstein and print its total word count."""
 
+import sys
 from stats import count_words
 from stats import char_count
+from stats import sort_char
 
 
 def get_book_text(filepath: str) -> str:
@@ -16,9 +18,23 @@ def main() -> None:
     filepath = "books/frankenstein.txt"
     contents = get_book_text(filepath)
     num_words = count_words(contents)
-    print(f"Found {num_words} total words")
     num_char = char_count(contents)
-    print(num_char)
+    sorted_report = sort_char(num_char)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for let in sorted_report:
+        # print(f"let {let}")
+        remove = let["char"]
+        # print(remove)
+        if remove.isalpha() == True:
+            removed = remove
+            print(f"{removed}: {let["num"]}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -7,7 +7,7 @@ def count_words(contents: str) -> int:
     return len(words)
 
 
-def char_count(contents: str) -> dict:
+def char_count(contents: str) -> dict[str, int]:
     """Returns the number of times each character appears in the string."""
     lower = contents.lower()
     characters = {}
@@ -17,3 +17,28 @@ def char_count(contents: str) -> dict:
         else:
             characters[char] += 1
     return characters
+
+
+def sort_on(item: dict) -> list:
+    """Takes a dictionary and returns the value keys"""
+    return item["num"]
+
+
+def sort_char(provide_dict: dict) -> list[dict]:
+    """Takes a dictionary and returns a sorted list of dictionaries"""
+    list_dict = []
+    for letter in provide_dict:
+        letters = {}
+        value = provide_dict[letter]
+        letters["char"] = letter
+        letters["num"] = value
+        list_dict.append(letters)
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+
+
+# if __name__ == "__main__":
+#     test_chars = {"a": 10, "b": 3, "c": 7}
+#     result = sort_char(test_chars)
+
+#     print(result)
